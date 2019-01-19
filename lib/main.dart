1617e25b29c9c1e20cdb5389a33669a6cafbcaf5
Pramono <pramono.winata@tokopedia.com>
2019-01-19 23:29:38 +0700

    Add navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'secondApp.dart';
 
 void main() => runApp(MyApp());
 
@@ -58,28 +59,16 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void decreaseCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter--;
-    });
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SecondApp()),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
@@ -107,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
               textColor: Colors.white,
               color: Colors.blue,
               onPressed: decreaseCounter,
-              child: new Text("Decrease"),
+              child: new Text("Enter new screen"),
             ),
             Text('You have pushed the button this many times:',),
             Text('$_counter', style: Theme.of(context).textTheme.display1,),
