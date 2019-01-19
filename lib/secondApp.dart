8385198d745228e4391c375db1d46c15fdb2f117
Pramono <pramono.winata@tokopedia.com>
2019-01-19 23:43:00 +0700

    toast example (?)

--- lib/secondApp.dart
+++ lib/secondApp.dart
@@ -5,18 +5,38 @@ import 'package:flutter/material.dart';
 class SecondApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    void goBack() {
+      Navigator.pop(context);
+    }
+
+    void makeToast(BuildContext context) {
+      Scaffold.of(context).showSnackBar(new SnackBar(
+        content: new Text("Your toast here"),
+      ));
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Second Route"),
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
-        ),
+      body: Builder(
+        builder:(context) => Center (
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          RaisedButton(
+            onPressed: goBack,
+            child: Text('Go back!'),
+          ),
+          RaisedButton(
+            onPressed: () {makeToast(context);},
+            child: Text('Make a toast!'),
+          ),
+        ],
       ),
+      ),
+    )
     );
   }
 }
